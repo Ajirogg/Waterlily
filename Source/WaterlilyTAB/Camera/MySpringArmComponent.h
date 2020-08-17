cc5cd0a6515a5605b4819a923e3687bfe2fce42b
Ajirogg <raimundo.guillaume@gmail.com>
2020-08-17 20:41:14 +0200

    Remise en ligne du code

---
+++ Source/WaterlilyTAB/Camera/MySpringArmComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "MySpringArmComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WATERLILYTAB_API UMySpringArmComponent : public USpringArmComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
